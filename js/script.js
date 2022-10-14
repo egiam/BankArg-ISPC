0289b39eea5ad1e97ce881170b964d9855f64794
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-10-13 17:25:08 -0700

    Mejora y Actualizacion del Inicio de Sesion

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
     navLinks.style.right = "0";
+    // dysplay: block
+    // navLinks.style.display = "block";
 }
 
 function hideMenu() {
     navLinks.style.right = "-200px";
+    // dysplay: none
+    // navLinks.style.display = "none";
 }
