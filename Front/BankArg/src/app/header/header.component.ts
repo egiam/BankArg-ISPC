f5bac0db41e8f0122c483d310cd67ed966d1618c
Joaco <joacobonvechi2272002@gmail.com>
2023-04-19 19:49:28 -0300

    sidebar funcionando

--- Front/BankArg/src/app/header/header.component.ts
+++ Front/BankArg/src/app/header/header.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  validacion = false
+
+  public change() {
+    if (this.validacion === true){
+     this.validacion = false
+  }
+    else{
+      this.validacion = true
+  }
+
+}
+}
