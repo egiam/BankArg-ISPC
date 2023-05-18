43dc4f6467148ff121904d17e2124863180ed030
Peralta María Laura <peraltaml.ispc@gmail.com>
2023-05-18 17:06:45 -0300

    Empezando con Django

---
+++ Back/bankarg/modulos/banco/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BancoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'banco'
---
+++ Back/bankarg/modulos/banco/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Cuenta(models.Model):
+    pass
+
+class 
