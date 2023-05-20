9562f5111f3ec602fcdb49f13e531537360af148
Joaquin Bonvechi <joacobonvechi2272002@gmail.com>
2023-05-20 17:36:38 -0300

    pagos de servicios service agregado

--- Front/BankArg/src/app/vistas/pagos/pagos.component.ts
+++ Front/BankArg/src/app/vistas/pagos/pagos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Persona } from 'src/app/modelos/persona';
+import { PagarServiciosService } from 'src/app/servicios/services/pagar-servicios.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -7,6 +9,10 @@ import Swal from 'sweetalert2';
   styleUrls: ['./pagos.component.css']
 })
 export class PagosComponent {
+  constructor(public pagarS:PagarServiciosService){
+  }
+
+  persona1 = new Persona(1, 'admin', 10000.5, 'admin', '10101010');
 
 public NoDisponible() {
   Swal.fire({
@@ -17,6 +23,23 @@ public NoDisponible() {
       confirmButtonText:"Aceptar"
   });
 };
+
+public async PagoDeServicio(){
+  const { value: servicio } = await Swal.fire({
+    title: "Servicio",
+    text: "¿Que servicio desea pagar?",
+    input:"select",
+    inputOptions:{
+      agua:"Agua",
+      luz:"Luz",
+      gas:"Gas"
+    },
+    showConfirmButton:true,
+    confirmButtonText:"Aceptar"
+});
+  this.pagarS.PagarServiciosS(this.persona1,servicio)
+
+}
 
 
 }
