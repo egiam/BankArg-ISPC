2f8988e3e3d82d821527ab0a16a59dbde8482757
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2023-05-31 09:24:04 -0700

    Update settings.py

--- Back/bankarg/bankarg/settings.py
+++ Back/bankarg/bankarg/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         "USER": "root",
         "PASSWORD": "",
         "HOST": "localhost",
-        "PORT": "3308",
+        "PORT": "3306",
         "OPTIONS": {
             "sql_mode": "traditional",
         },
