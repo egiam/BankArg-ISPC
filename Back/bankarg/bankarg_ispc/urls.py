c00b2f5e82519d0b98da8fd3721441e1835b249f
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2023-05-31 13:51:59 -0700

    Update

--- Back/bankarg/bankarg_ispc/urls.py
+++ Back/bankarg/bankarg_ispc/urls.py
@@ -9,10 +9,14 @@ from rest_framework.routers import DefaultRouter
 from bankarg_ispc.views import LoginView, LogoutView, SignupView, RegisterView
 from bankarg_ispc import views
 
+# from knox import views as knox_views
+
 
 urlpatterns = [
     path("auth/login/", LoginView.as_view(), name="auth_login"),
     path("auth/logout/", LogoutView.as_view(), name="auth_logout"),
+    # path("auth/logout/", knox_views.LogoutView.as_view(), name="auth_logout"),
+    # path("auth/logoutall/", knox_views.LogoutAllView.as_view(), name="auth_logoutall"),
     path("auth/signup/", SignupView.as_view(), name="auth_signup"),
     path("auth/register/", RegisterView.as_view(), name="auth_register"),
     # path("api-auth/", include("rest_framework.urls")),
--- Back/bankarg/bankarg_ispc/views.py
+++ Back/bankarg/bankarg_ispc/views.py
@@ -9,6 +9,21 @@ from knox.models import AuthToken
 from .serializers import UserSerializer, RegisterSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import permissions
+
+# from rest_framework.authtoken.serializers import AuthTokenSerializer
+# from knox.views import LoginView as KnoxLoginView
+
+
+# class LoginView(KnoxLoginView):
+#     permission_classes = (permissions.AllowAny,)
+
+# def post(self, request, format=None):
+#     serializer = AuthTokenSerializer(data=request.data)
+#     serializer.is_valid(raise_exception=True)
+#     user = serializer.validated_data["user"]
+#     login(request, user)
+#     return super(LoginView, self).post(request, format=None)
 
 
 class LoginView(APIView):
