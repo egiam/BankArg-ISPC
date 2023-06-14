08d02eb8957b77422ec54029d411e5c2c88e83d6
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2023-06-14 15:40:49 -0700

    Mejoras de Alerts en personas

--- Front/BankArg/src/app/vistas/body/personas/personas.component.ts
+++ Front/BankArg/src/app/vistas/body/personas/personas.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/servicios/api/api.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-personas',
@@ -14,27 +15,67 @@ export class PersonasComponent {
 
   // editar
   editarButton(id: number, data: any) {
-    this.cuenta.putPersonas(id, data).subscribe(() => {
-      console.log('Persona editada correctamente');
-      this.router
-        .navigateByUrl('/RefreshComponent', { skipLocationChange: true })
-        .then(() => {
-          this.router.navigate(['personas']);
+    this.cuenta.putPersonas(id, data).subscribe(
+      (res) => {
+        console.log('Persona editada correctamente');
+        Swal.fire({
+          title: 'Editado',
+          text: 'Persona editada correctamente',
+          icon: 'success',
+          showConfirmButton: true,
+          confirmButtonText: 'Aceptar',
+        }).then(() => {
+          this.router
+            .navigateByUrl('/RefreshComponent', { skipLocationChange: true })
+            .then(() => {
+              this.router.navigate(['personas']);
+            });
         });
-    });
+      },
+      (err) => {
+        console.log(err);
+        Swal.fire({
+          title: 'Error',
+          text: 'Datos insuficientes o Incorrectos',
+          icon: 'error',
+          showConfirmButton: true,
+          confirmButtonText: 'Aceptar',
+        });
+      }
+    );
   }
 
   // eliminar
   eliminarButton(id: number) {
     console.log(id);
-    this.cuenta.deletePersonas(id).subscribe(() => {
-      console.log('Persona eliminada correctamente');
-      this.router
-        .navigateByUrl('/RefreshComponent', { skipLocationChange: true })
-        .then(() => {
-          this.router.navigate(['personas']);
+    this.cuenta.deletePersonas(id).subscribe(
+      (res) => {
+        console.log('Persona eliminada correctamente');
+        Swal.fire({
+          title: 'Eliminado',
+          text: 'Persona eliminada correctamente',
+          icon: 'success',
+          showConfirmButton: true,
+          confirmButtonText: 'Aceptar',
+        }).then(() => {
+          this.router
+            .navigateByUrl('/RefreshComponent', { skipLocationChange: true })
+            .then(() => {
+              this.router.navigate(['personas']);
+            });
         });
-    });
+      },
+      (err) => {
+        console.log(err);
+        Swal.fire({
+          title: 'Error',
+          text: 'No se pudo eliminar la persona',
+          icon: 'error',
+          showConfirmButton: true,
+          confirmButtonText: 'Aceptar',
+        });
+      }
+    );
   }
 
   nuevaPersona: any = {};
@@ -42,13 +83,33 @@ export class PersonasComponent {
   // crear
   crearButton(data: any) {
     console.log(data);
-    this.cuenta.postPersonas(data).subscribe(() => {
-      console.log('Persona creada correctamente');
-      this.router
-        .navigateByUrl('/RefreshComponent', { skipLocationChange: true })
-        .then(() => {
-          this.router.navigate(['personas']);
+    this.cuenta.postPersonas(data).subscribe(
+      (res) => {
+        console.log('Persona creada correctamente');
+        Swal.fire({
+          title: 'Creado',
+          text: 'Persona creada correctamente',
+          icon: 'success',
+          showConfirmButton: true,
+          confirmButtonText: 'Aceptar',
+        }).then(() => {
+          this.router
+            .navigateByUrl('/RefreshComponent', { skipLocationChange: true })
+            .then(() => {
+              this.router.navigate(['personas']);
+            });
         });
-    });
+      },
+      (err) => {
+        console.log(err);
+        Swal.fire({
+          title: 'Error',
+          text: 'Datos insuficientes o Incorrectos',
+          icon: 'error',
+          showConfirmButton: true,
+          confirmButtonText: 'Aceptar',
+        });
+      }
+    );
   }
 }
